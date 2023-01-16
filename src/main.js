90a8316d43de515d98b8a78971c7a44e2b0dd1c3
Gorobchuk Alex <5267720@gmail.com>
2023-01-16 08:15:32 +0200

    v.1.3

--- src/main.js
+++ src/main.js
@@ -1,46 +1,45 @@
 // import { encodeQueryData } from "./f.js";
-function encodeQueryData(data) {
+const encodeQueryData = data => {
 	let ret = [];
 	for (let d in data)
-		ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
-
-	return ret.join("&");
+		ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+	return ret.join('&');
 }
 
-const searchStreet = (street) => {
-	const basemap = "https://nominatim.openstreetmap.org/search.php?",
-		// search = document.querySelector("#street-search"),
-		// result = document.querySelector(".result"),
-		resultContent = document.querySelector(".result-content");
+const searchStreet = street => {
+	const basemap = 'https://nominatim.openstreetmap.org/search.php?';
+	// const search = document.querySelector("#street-search");
+	// const result = document.querySelector(".result");
+	const resultContent = document.querySelector('.result-content');
 
 	const url = {
 		street: street,
-		city: "Київ",
-		format: "jsonv2",
+		city: 'Київ',
+		format: 'jsonv2',
 		addressdetails: 1,
 		namedetails: 1,
 		extratags: 1,
-		"accept-language": "uk",
+		'accept-language': 'uk',
 		limit: 15,
-		email: "5267720@gmail.com",
+		email: '5267720@gmail.com',
 		dedupe: 1
 	};
 
 	fetch(basemap + encodeQueryData(url))
-		.then((response) => response.json())
-		.then((data) => {
-			// console.log(data);
-			if (data !== "") {
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			/*if (data !== '') {
 				data.forEach((e) => {
 					resultContent.innerHTML = '<div class="res-item">' +
 						'<span class="s-name">' + e["namedetails"]["name"] + '</span>' +
 						'<span class="s-divider"> - </span>' +
-						'<span class="s-old_name">' + e["namedetails"]["old_name"] + '</span>'
+						'<span class="s-old_name">' + (e["namedetails"]["old_name"] ?? 'Nevidomo') + '</span>'
 					'</div>';
 				});
 			} else {
 				resultContent.innerHTML = '<span style="color:red">Error search</span>';
-			}
+			}*/
 		});
 
 	// console.log(basemap + encodeQueryData(url));
